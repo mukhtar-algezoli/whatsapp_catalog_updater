11f136f5064a8356d5de3161757492622592ce6b
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2022-03-08 14:20:21 +0200

    adding updater file

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 client = MongoClient("mongodb+srv://enigma:A_pass2mongo@sme-platform.pkbgh.mongodb.net")
-products = client.backend.products.find({"owner":ObjectId(str("61f96cbffa391f211a7dd540"))})
-products = list(products)
-print(products)
+
+whatsapp_users = client.backend.users.find({ "whatsapp_update_file" : { "$exists" : True } })
+for user in whatsapp_users:
+    products = client.backend.products.find({"owner":user["_id"]})
+    products = list(products)
+    print(products)
+
