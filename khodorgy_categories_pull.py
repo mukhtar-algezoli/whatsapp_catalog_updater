df89d6756f20dafdeb568c674d67c2643af45ab7
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2022-03-09 00:37:52 +0200

    adding khodorgy get categories

---
+++ khodorgy_categories_pull.py
@@ -0,0 +1,31 @@
+from re import X
+from pymongo import MongoClient
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+import requests
+import re
+
+from bson.objectid import ObjectId
+
+client = MongoClient("mongodb+srv://enigma:A_pass2mongo@sme-platform.pkbgh.mongodb.net")
+
+whatsapp_users = client.backend.users.find({ "whatsapp_update_file" : { "$exists" : True } })
+
+
+def get_khodorgy_categories():
+    endpoint = "https://khodorgy.com/api/categories"
+    headers = {
+    'accept': 'application/json',
+    'api_token': "u5XON0EPMNlKCehRvIT",
+    'lang': 'ar'
+        }
+    res = requests.get(endpoint,
+                            headers=headers)
+
+    res_data =  res.json()["data"]
+    
+    return res_data
+
+
+print(get_khodorgy_categories())
