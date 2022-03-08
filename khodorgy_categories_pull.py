78c570a59fff01c7a0a3fe7595ce7270e010cbe5
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2022-03-09 01:14:19 +0200

    adding khodorgy get categories

--- khodorgy_categories_pull.py
+++ khodorgy_categories_pull.py
@@ -2,10 +2,11 @@ from re import X
 from pymongo import MongoClient
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from bson.objectid import ObjectId
 import requests
 import re
+from datetime import datetime
 
-from bson.objectid import ObjectId
 
 client = MongoClient("mongodb+srv://enigma:A_pass2mongo@sme-platform.pkbgh.mongodb.net")
 
@@ -31,5 +32,19 @@ cats_ar = get_khodorgy_categories("ar")
 cats_en = get_khodorgy_categories("en")
 
 for i in range(0,4):
-    print(i)
+        user_dict = {}
+        user_dict["owner"] = ObjectId("620ce3dc422c0f83d7740591")
+        user_dict["name"] = cats_en[i]["name"]
+        user_dict["desc"] = "khodorgy category"
+        user_dict["arabicName"] = cats_ar[i]["name"]
+        user_dict["arabicDesc"] = "khodorgy category"
+        user_dict["root"] = True
+        user_dict["children"]= []
+        user_dict["thumbnail"] = None
+        user_dict["images"] = []
+        user_dict["createdAt"] = datetime.now()
+        user_dict["updatedAt"] = datetime.now()
+        user_dict["__v"]= 0
+        
+        client.backend.categories.insert_one(user_dict)
 
