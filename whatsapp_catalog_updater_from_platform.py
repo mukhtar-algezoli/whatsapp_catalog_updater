134ff34aca8fdbedad30f3eb95020423154aa7ef
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2022-03-14 21:33:23 +0200

    adding catalog platform updater

---
+++ whatsapp_catalog_updater_from_platform.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import pandas
+import gspread
+import pandas as pd
+import gspread_dataframe as gd
+
+from oauth2client.service_account import ServiceAccountCredentials
+# define the scope
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+
+# add credentials to the account
+creds = ServiceAccountCredentials.from_json_keyfile_name('/content/semiotic-bloom-334320-53016b79b117.json', scope)
+
+# authorize the clientsheet 
+client = gspread.authorize(creds)
+df = pd.DataFrame()
+mongo = MongoClient("mongodb+srv://enigma:A_pass2mongo@sme-platform.pkbgh.mongodb.net")
+users = mongo.backend.users.find({"whatsapp.catalog_feed_name": { "$exists": True } } )
+for user in users:
+  sheet = client.open(user["whatsapp"]["catalog_feed_name"])
+  sheet_instance = sheet.get_worksheet(0)
+  df = pd.DataFrame()  
+  sheet_instance.resize(rows=1)
+  products = mongo.backend.products.find({"owner": user["_id"]})
+  for product in products:
+    additional_images = joined_string = ",".join(product["images"])
+    data = {"id":str(product["_id"]),	"title":product["name"],	"description":product["name"],	"google_product_category":"product",	"product_type":"product",	"link":"https://khodorgy.com/",	"image_link":product["thumbnail"] if product["thumbnail"]!=None else "https://i.imgur.com/RKl8LpW.png",	"condition":"new",	"availability":"in stock",	"price":product["price"],	"sale_price":" ",	"sale_price_effective_date":" ",	"gtin":str(product["categories"][0]),	"brand":"no brand",	"item_group_id":str(product["categories"][0]),	"gender":" ",	"age_group":" ",	"color":" ",	"size":" ",	"shipping":" ",	"custom_label_0":" ", "additional_image_link":additional_images}
+    # data = {"id":str(product["_id"]),	"title":product["name"],	"description":"some description",	"google_product_category":"product",	"product_type":"product",	"link":"https://khodorgy.com/",	"image_link":"https://i.imgur.com/RKl8LpW.png",	"condition":"new",	"availability":"in stock",	"price":product["price"],	"sale_price":" ",	"sale_price_effective_date":" ",	"gtin":str(product["categories"][0]),	"brand":"no brand",	"item_group_id":str(product["categories"][0]),	"gender":" ",	"age_group":" ",	"color":" ",	"size":" ",	"shipping":" ",	"custom_label_0":" ", "additional_image_link":additional_images}
+    df = df.append(data, ignore_index=True)
+  # Connecting with `gspread` here
+  ws = client.open(user["whatsapp"]["catalog_feed_name"]).get_worksheet(0)
+  existing = gd.get_as_dataframe(ws)
+  updated = existing.append(df)
+  gd.set_with_dataframe(ws, updated)
