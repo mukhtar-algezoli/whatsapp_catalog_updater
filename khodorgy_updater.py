810e69e796781f28fbe78d2bd9303f60a2340a0d
mukhtar-algezoli <38084259+mukhtar-algezoli@users.noreply.github.com>
2022-06-28 00:27:07 +0200

    debugging

--- khodorgy_updater.py
+++ khodorgy_updater.py
@@ -158,7 +158,7 @@ while True:
                         updated_product = client.backend.products.find_one_and_update({'name': products_ar[i]["name"] + "\\"+ khodorgy_unit_name(products_ar[i]["unit_id"])+ " " +products_ar[i]["weight_unit"]["title"], "owner": ObjectId("620ce3dc422c0f83d7740591")},{'$set': {"price": float(products_ar[i]["price"]) * khodorgy_unit_value(products_ar[i]["unit_id"])}}, upsert=False)
                         updated_product = client.backend.products.find_one_and_update({'name': products_ar[i]["name"] + "\\"+ khodorgy_unit_name(products_ar[i]["unit_id"])+ " " +products_ar[i]["weight_unit"]["title"], "owner": ObjectId("620ce3dc422c0f83d7740591")},{'$set': {"khodorgy_id": products_ar[i]["id"]}}, upsert=False)  
                         print("product updated")
-                        print(products_ar[i]["price"]) * khodorgy_unit_value(products_ar[i]["unit_id"])
+                        print(products_ar[i]["price"]))
 
                     else:
                         print("product not found")
